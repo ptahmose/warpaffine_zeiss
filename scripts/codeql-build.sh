61df80d7a5e84c9db5f3853bcbda706213a62606
ptahmose <jbohl@h-quer.de>
2024-03-22 12:35:52 +0100

    cosmetic

--- scripts/codeql-build.sh
+++ scripts/codeql-build.sh
@@ -5,12 +5,12 @@
 
 # Check if exactly one argument was provided
 if [ "$#" -ne 1 ]; then
-    echo "Error: This script requires exactly one argument. This argument is the build folder."
-    exit 1
+     echo "Error: This script requires exactly one argument. This argument is the build folder."
+     exit 1
 fi
 
 # download the key to system keyring
-wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB| gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
 # add signed entry to apt sources and configure the APT client to use Intel repository:
 echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
 # Update packages list and repository index
