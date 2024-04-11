c6299ef1f32a7296b37233bd30810a7629fe7df3
ptahmose <jbohl@h-quer.de>
2024-04-11 23:48:45 +0200

    fix linter issue

--- libwarpaffine/sliceswriter/SlicesWriterTbb.h
+++ libwarpaffine/sliceswriter/SlicesWriterTbb.h
@@ -41,8 +41,8 @@ public:
     void AddSlice(const AddSliceInfo& add_slice_info) override;
 
     void Close(const std::shared_ptr<libCZI::ICziMetadata>& source_metadata,
-               const libCZI::ScalingInfo* new_scaling_info,
-               const std::function<void(libCZI::IXmlNodeRw*)>& tweak_metadata_hook) override;
+                const libCZI::ScalingInfo* new_scaling_info,
+                const std::function<void(libCZI::IXmlNodeRw*)>& tweak_metadata_hook) override;
 
 private:
     void WriteWorker();
